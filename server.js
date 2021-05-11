ea83489f9431914351b6909b2f94938b2e1c5b8c
David Akin-James <davidakinjames@yahoo.com>
2021-05-11 16:47:16 +0100

    modified the package.json file and the server.js file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+require('dotenv').config();
+const { PORT } = process.env;
+const port = process.env ||  PORT;
+
 
 //Setting Up Mongoose 
 const mongoose = require('mongoose');
