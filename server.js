11affe2b9b4eab1787034dbc9ca4b4ee033ea0c5
David Akin-James <davidakinjames@yahoo.com>
2021-05-11 15:26:27 +0100

    Finished Application

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/*  TASKS 
-  1.  A POST request to /users to create a new user.
-  2.  A GET request to /users to fetch all users.
-  3.  A PUT request to users/:id to update a single user
-  4.  A DELETE request to /books/:id to delete
-*/
-
-
 
 const express = require('express');
 const app = express();
@@ -40,11 +32,10 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
+//POST ROUTE
 //POST request to /users to create a new user
 app.post('/users', (req,res)=> {
    //Retrieve new book from req.body
-  
-
    User.create({
      name: req.body.name,
      email: req.body.email,
@@ -59,8 +50,69 @@ app.post('/users', (req,res)=> {
 
 })
 
+//GET ROUTE
+// GET request to /users to fetch all users
+app.get('/users', (req, res)=> {
+  //Fetching all users
+  User.find({}, (err, users) => {
+    if(err) {
+      return res.status(500).json({ message: err })
+    } else {
+      return res.status(200).json({message: users})
+    }
+  })
+})
+//GET request to fetch a single user
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id, (err, user)=> {
+    if (err) {
+      return res.status(500).json({ message: err})
+    }
+     else if (!user) {
+       return res.status(404).json({ message: "user not found"})
+     } 
+     else {
+      return res.status(200).json({ user })
+    }
+  })
+})
 
+//PUT request to /users/id to update a single user
+app.put('/users/:id', (req, res)=> {
+  User.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    email:req.body.email,
+    country: req.body.country
+  }, (err, user) => {
+     if (err) {
+      return res.status(500).json({message: err})
+    } else  if (!user) {
+     return res.status(404).json({message: "book not found"})
+    } else {
+      user.save((err, savedUser)=> {
+        if (err) {
+          return res.status(400).json({message: err})
+        } else {
+          return res.status(200).json({message: "user updated successfully" })
+        }
+      })
+    }
+  })
+})
 
+//DELETE request to /users/:id to delete
+app.delete('/users/:id', (req, res)=> {
+  User.findByIdAndDelete(req.params.id, (err, user) =>{
+    if(err) {
+      return res.status(500).json({message: err})
+    } else if (!user) {
+      return res.status(404).json({message: "user not found"})
+    }
+    else {
+      return res.status(200).json({message: "user deleted successfully"})
+    }
+  })
+})
 
 
 
